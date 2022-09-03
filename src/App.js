a908bfcd2b2824ef041560592d510cad3bb27826
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2022-09-03 09:25:43 +0530

    fix bug

--- src/App.js
+++ src/App.js
@@ -4,17 +4,29 @@ import {useState} from 'react';
 
 function App() {
 
-  var [count, SetCount] = useState(0);
+  var [number, SetNumber] = useState(0);
+
+  const incr = ()=>{
+    SetNumber(c =>c+1)
+  };
+
+  const decr = ()=>{
+    SetNumber(c => c-1);
+  }
+
+  const reset = () => {
+    SetNumber(0);
+  }
   return (
     <div className="App">
     
     <h1>Towheed</h1>
     <p>Tawakal, Taqwa , Tavazo</p>
 
-    <h2>Count {count}</h2>
-    <button onClick={e=>setCount(c => c+1)}>+</button>
-    <button onClick={e=>setCount(c => c-1)}>-</button>
-    <button onClick={e=>setCount(0)}>0</button>
+    <h2>Count {number}</h2>
+    <button onClick={incr}>+</button>
+    <button onClick={decr}>-</button>
+    <button onClick={reset}>0</button>
     </div>
   );
 }
